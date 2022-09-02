2f074e7556cb7a51cb76c094c0940e9e20b55464
Toffee-iZt <mgsiask@gmail.com>
2022-09-02 22:13:56 +0300

    add games support

---
+++ games.go
@@ -0,0 +1,37 @@
+package bot
+
+import "tghwbot/bot/tg"
+
+// Game contains information about the game to be sent.
+type Game struct {
+	ShortName string
+}
+
+func (g Game) params(p params) {
+	p.set("game_short_name", g.ShortName)
+}
+
+// SetGameScore contains parameters for setting the game score.
+type SetGameScore struct {
+	Score       int
+	Force       bool
+	DisableEdit bool
+}
+
+// SetGameScore sets the score of the specified user in a game message.
+func (c Context) SetGameScore(sig *MessageSignature, userID int64, s SetGameScore) (*tg.Message, error) {
+	p := params{}
+	p.setInt64("user_id", userID)
+	p.setInt("score", s.Score)
+	p.setBool("force", s.Force)
+	p.setBool("disable_edit_message", s.DisableEdit)
+	return c.sig(sig, "setGameScore", p)
+}
+
+// GetGameHighScores returns data for high score tables.
+func (c Context) GetGameHighScores(sig *MessageSignature, userID int64) ([]tg.GameHighScore, error) {
+	p := params{}
+	sig.signature(p)
+	p.setInt64("user_id", userID)
+	return api[[]tg.GameHighScore](c, "getGameHighScores", p)
+}
