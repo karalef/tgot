7578ce080f12aa98afd61586de0cf592110a7bde
Toffee-iZt <mgsiask@gmail.com>
2022-09-02 22:05:02 +0300

    improve errors and default methods

--- api.go
+++ api.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
@@ -17,13 +18,33 @@ import (
 
 // Error type.
 type Error struct {
-	Err      error
-	Method   string
-	Response []byte
+	Method string
+	Params params
+	Err    tg.APIError
+	cancel bool
 }
 
 func (e *Error) Error() string {
-	return e.Err.Error()
+	return fmt.Sprintf("%s\n%s %v", e.Err.Error(), e.Method, e.Params)
+}
+
+func (e *Error) Unwrap() error {
+	return e.Err
+}
+
+type jsonError struct {
+	Method   string
+	Params   params
+	Response []byte
+	Err      error
+}
+
+func (e *jsonError) Error() string {
+	return fmt.Sprintf("%s\n%s %v\nresponse:\n%s", e.Err.Error(), e.Method, e.Params, string(e.Response))
+}
+
+func (e *jsonError) Unwrap() error {
+	return e.Err
 }
 
 type params url.Values
@@ -75,10 +96,27 @@ func (p params) setJSON(key string, v interface{}) {
 	}
 }
 
-func performRequest[T any](b *Bot, method string, p params, files ...File) (T, error) {
+type file struct {
+	field string
+	tg.FileSignature
+}
+
+func performRequestContext[T any](ctx context.Context, b *Bot, method string, p params, files ...file) (T, error) {
 	var ctype = "application/x-www-form-urlencoded"
 	var data io.Reader
-	if files == nil {
+	upload := false
+	for i := range files {
+		if _, r := files[i].FileData(); r != nil {
+			upload = true
+			break
+		}
+	}
+
+	if !upload {
+		for i := range files {
+			urlid, _ := files[i].FileData()
+			p.set(files[i].field, urlid)
+		}
 		data = strings.NewReader(url.Values(p).Encode())
 	} else {
 		ctype, data = writeMultipart(p, files)
@@ -86,17 +124,17 @@ func performRequest[T any](b *Bot, method string, p params, files ...File) (T, e
 
 	var nilResult T
 	u := b.apiURL + b.token + "/" + method
-	req, err := http.NewRequestWithContext(b.ctx, http.MethodPost, u, data)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, u, data)
 	if err != nil {
 		return nilResult, err
 	}
 	req.Header.Set("Content-Type", ctype)
 	resp, err := b.client.Do(req)
-	switch e := errors.Unwrap(err); e {
-	case context.Canceled, context.DeadlineExceeded:
-		return nilResult, e
-	default:
-		if err != nil {
+	if err != nil {
+		switch e := errors.Unwrap(err); e {
+		case context.Canceled, context.DeadlineExceeded:
+			return nilResult, e
+		default:
 			return nilResult, err
 		}
 	}
@@ -104,20 +142,31 @@ func performRequest[T any](b *Bot, method string, p params, files ...File) (T, e
 
 	r, raw, err := internal.DecodeJSON[tg.APIResponse[T]](resp.Body)
 	if err != nil {
-		return nilResult, &Error{
-			Err:      err,
+		return nilResult, &jsonError{
 			Method:   method,
+			Params:   p,
 			Response: raw,
+			Err:      err,
 		}
 	}
 	if r.APIError != nil {
-		return nilResult, &Error{
-			Err:    r.APIError,
+		e := &Error{
 			Method: method,
+			Params: p,
+			Err:    *r.APIError,
 		}
+		if e.Err.Code == http.StatusUnauthorized {
+			b.cancel(e.Err)
+			e.cancel = true
+		}
+		return nilResult, e
 	}
 
 	return r.Result, nil
+}
+
+func performRequest[T any](b *Bot, method string, p params, files ...file) (T, error) {
+	return performRequestContext[T](context.Background(), b, method, p, files...)
 }
 
 func performRequestEmpty(b *Bot, method string, p params) error {
@@ -125,7 +174,7 @@ func performRequestEmpty(b *Bot, method string, p params) error {
 	return err
 }
 
-func writeMultipart(p params, files []File) (string, io.Reader) {
+func writeMultipart(p params, files []file) (string, io.Reader) {
 	r, w := io.Pipe()
 	mp := multipart.NewWriter(w)
 	go func() {
@@ -140,8 +189,9 @@ func writeMultipart(p params, files []File) (string, io.Reader) {
 		}
 
 		for _, file := range files {
-			if d := file.Data(); d != "" {
-				err := mp.WriteField(file.Field, d)
+			if _, ok := file.FileSignature.(*tg.InputFile); !ok {
+				urlid, _ := file.FileData()
+				err := mp.WriteField(file.field, urlid)
 				if err != nil {
 					w.CloseWithError(err)
 					return
@@ -149,11 +199,14 @@ func writeMultipart(p params, files []File) (string, io.Reader) {
 				continue
 			}
 
-			name, reader := file.UploadData()
-			part, err := mp.CreateFormFile(file.Field, name)
+			name, reader := file.FileData()
+			part, err := mp.CreateFormFile(file.field, name)
 			if err != nil {
 				w.CloseWithError(err)
 				return
+			}
+			if reader == nil {
+				continue
 			}
 			_, err = io.Copy(part, reader)
 			if err != nil {
@@ -165,12 +218,6 @@ func writeMultipart(p params, files []File) (string, io.Reader) {
 	return mp.FormDataContentType(), r
 }
 
-// File contains field and file data.
-type File struct {
-	Field string
-	*tg.InputFile
-}
-
 func (b *Bot) downloadFile(path string) (io.ReadCloser, error) {
 	resp, err := b.client.Get(b.fileURL + b.token + "/" + path)
 	if err != nil {
@@ -179,26 +226,33 @@ func (b *Bot) downloadFile(path string) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
-func (b *Bot) getMe() (*tg.User, error) {
+// GetMe returns basic information about the bot in form of a User object.
+func (b *Bot) GetMe() (*tg.User, error) {
 	return performRequest[*tg.User](b, "getMe", nil)
 }
 
-func (b *Bot) logOut() (bool, error) {
-	return performRequest[bool](b, "logOut", nil)
+// LogOut method.
+//
+// Use this method to log out from the cloud Bot API server before launching the bot locally.
+func (b *Bot) LogOut() error {
+	return performRequestEmpty(b, "logOut", nil)
 }
 
-func (b *Bot) close() (bool, error) {
-	return performRequest[bool](b, "close", nil)
+// Close method.
+//
+// Use this method to close the bot instance before moving it from one local server to another.
+func (b *Bot) Close() error {
+	return performRequestEmpty(b, "close", nil)
 }
 
-func (b *Bot) getUpdates(offset, timeout int, allowed ...string) ([]tg.Update, error) {
+func (b *Bot) getUpdates(ctx context.Context, offset, timeout, limit int, allowed []string) ([]tg.Update, error) {
 	p := params{}
 	p.setInt("offset", offset)
-	//p.setInt("limit", limit)
+	p.setInt("limit", limit)
 	p.setInt("timeout", timeout)
 	p.setJSON("allowed_updates", allowed)
 
-	return performRequest[[]tg.Update](b, "getUpdates", p)
+	return performRequestContext[[]tg.Update](ctx, b, "getUpdates", p)
 }
 
 type commandParams struct {
@@ -213,9 +267,7 @@ func (p *commandParams) params() params {
 	}
 	v := params{}
 	v.set("language_code", p.Lang)
-	if p.Scope != nil {
-		v.setJSON("scope", p.Scope)
-	}
+	v.setJSON("scope", p.Scope)
 	v.setJSON("commands", p.Commands)
 	return v
 }
@@ -240,15 +292,36 @@ func (b *Bot) deleteCommands(s tg.CommandScope, lang string) error {
 	return performRequestEmpty(b, "deleteMyCommands", p.params())
 }
 
-func (b *Bot) setDefaultAdminRights(rights *tg.ChatAdministratorRights, forChannels bool) error {
+// SetDefaultAdminRights changes the default administrator rights requested by the bot
+// when it's added as an administrator to groups or channels.
+func (b *Bot) SetDefaultAdminRights(rights *tg.ChatAdministratorRights, forChannels bool) error {
 	p := params{}
 	p.setJSON("rights", rights)
 	p.setBool("for_channels", forChannels)
 	return performRequestEmpty(b, "setMyDefaultAdministratorRights", p)
 }
 
-func (b *Bot) getDefaultAdminRights(forChannels bool) (*tg.ChatAdministratorRights, error) {
+// GetDefaultAdminRights returns the current default administrator rights of the bot.
+func (b *Bot) GetDefaultAdminRights(forChannels bool) (*tg.ChatAdministratorRights, error) {
 	p := params{}
 	p.setBool("for_channels", forChannels)
 	return performRequest[*tg.ChatAdministratorRights](b, "getMyDefaultAdministratorRights", p)
+}
+
+// SetDefaultChatMenuButton changes the bot's default menu button.
+//
+// This method is a wrapper for setChatMenuButton without specifying the chat id.
+// Full implementation of this method is available in the [Chat].
+func (b *Bot) SetDefaultChatMenuButton(menu tg.MenuButton) error {
+	p := params{}
+	p.setJSON("menu_button", menu)
+	return performRequestEmpty(b, "setChatMenuButton", p)
+}
+
+// GetDefaultChatMenuButton returns the current value of the bot's default menu button.
+//
+// This method is a wrapper for getChatMenuButton without specifying the chat id.
+// Full implementation of this method is available in the [Chat].
+func (b *Bot) GetDefaultChatMenuButton() (*tg.MenuButton, error) {
+	return performRequest[*tg.MenuButton](b, "getChatMenuButton", nil)
 }
