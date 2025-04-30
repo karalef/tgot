275dd5fdb078e5e7cb59b03eeaf5751b29655d28
karalef <mgsiask@gmail.com>
2025-04-30 15:18:08 +0300

    move passport under api

--- api/tgpassport/credentials.go
+++ api/tgpassport/credentials.go
@@ -1,5 +1,53 @@
 package tgpassport
 
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+
+	"github.com/karalef/tgot/api/tg"
+)
+
+// DecryptCredentials decrypts telegram encrypted passport credentials.
+func DecryptCredentials(ecreds tg.EncryptedCredentials, priv *rsa.PrivateKey, nonce string) (*Credentials, error) {
+	secret, err := decodeString(ecreds.Secret, "EncryptedCredentials.Secret")
+	if err != nil {
+		return nil, err
+	}
+	secret, err = rsa.DecryptOAEP(sha1.New(), nil, priv, secret, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	dataHash, err := decodeString(ecreds.Hash, "EncryptedCredentials.Hash")
+	if err != nil {
+		return nil, err
+	}
+	data, err := decodeString(ecreds.Data, "EncryptedCredentials.Data")
+	if err != nil {
+		return nil, err
+	}
+	data, err = decrypt(secret, dataHash, data)
+	if err != nil {
+		return nil, err
+	}
+
+	var creds Credentials
+	if err = json.Unmarshal(data, &creds); err != nil {
+		return nil, err
+	}
+	if creds.Nonce != nonce {
+		return nil, errors.New("nonce does not match")
+	}
+	return &creds, nil
+}
+
 // Credentials is a JSON-serialized object.
 type Credentials struct {
 	SecureData SecureData `json:"secure_data"`
@@ -39,9 +87,75 @@ type DataCredentials struct {
 	Secret   string `json:"secret"`
 }
 
+// DecryptData decrypts base64-encoded encrypted data.
+// dst must satisfy [DataType] interface.
+func (c DataCredentials) DecryptData(dst any, data string) error {
+	encryptedData, err := decodeString(data, "data")
+	if err != nil {
+		return err
+	}
+	dataJSON, err := decrypt([]byte(c.Secret), []byte(c.DataHash), encryptedData)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(dataJSON, dst)
+}
+
 // FileCredentials can be used to decrypt encrypted files from the front_side, reverse_side, selfie, files
 // and translation fields in EncryptedPassportElement.
 type FileCredentials struct {
 	FileHash string `json:"file_hash"`
 	Secret   string `json:"secret"`
+}
+
+// Decrypt decrypts encrypted file data.
+func (c FileCredentials) Decrypt(fileData []byte) ([]byte, error) {
+	return decrypt([]byte(c.Secret), []byte(c.FileHash), fileData)
+}
+
+func decrypt(secret, hash, data []byte) ([]byte, error) {
+	if len(secret) == 0 {
+		return nil, errors.New("empty secret")
+	}
+	if len(hash) != sha256.Size {
+		return nil, errors.New("hash size does not match sha256")
+	}
+	if len(data) == 0 || len(data)%16 != 0 {
+		return nil, errors.New("data length is not divisible by 16")
+	}
+
+	h := sha512.New()
+	h.Write(secret)
+	h.Write(hash)
+	dig := h.Sum(nil)
+
+	// decrypt data
+	block, err := aes.NewCipher(dig[0:32])
+	if err != nil {
+		panic(err)
+	}
+	cipher.NewCBCDecrypter(block, dig[32:48]).CryptBlocks(data, data)
+
+	// verify data
+	if dig := sha256.Sum256(data); string(dig[:]) != string(hash) {
+		return nil, errors.New("hash does not match")
+	}
+	padding := data[0]
+	if padding < 32 {
+		return nil, errors.New("invalid data padding")
+	}
+
+	return data[padding:], err
+}
+
+func decodeString(s, name string) ([]byte, error) {
+	if len(s) == 0 {
+		return nil, errors.New("empty " + name)
+	}
+	dec, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return nil, errors.New(
+			"invalid encoding of " + string(name) + " (must be base64)")
+	}
+	return dec, nil
 }
---
+++ api/tgpassport/request.go
@@ -0,0 +1,68 @@
+package tgpassport
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
+	"net/url"
+	"strconv"
+
+	"github.com/karalef/tgot/deeplinks"
+)
+
+// RequestParams contains parameters to request information.
+type RequestParams struct {
+	BotID int
+	Scope Scope
+
+	// Public key of the bot.
+	PublicKey *rsa.PublicKey
+
+	// Bot-specified nonce.
+	// For security purposes it should be a cryptographically secure unique identifier of the request.
+	Nonce string
+}
+
+// Query generates query part of the request URI.
+func (p *RequestParams) Query() (url.Values, error) {
+	if p.BotID == 0 || p.PublicKey == nil || p.Nonce == "" {
+		return nil, errors.New("all fields are required")
+	}
+	if p.Scope.V != 1 {
+		return nil, errors.New("invalid version")
+	}
+	scope, err := json.Marshal(p.Scope)
+	if err != nil {
+		return nil, err
+	}
+	key := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: x509.MarshalPKCS1PublicKey(p.PublicKey),
+	})
+	return url.Values{
+		"bot_id":     {strconv.Itoa(p.BotID)},
+		"scope":      {string(scope)},
+		"public_key": {string(key)},
+		"nonce":      {p.Nonce},
+	}, nil
+}
+
+// Deeplink generates request deeplink.
+func (p *RequestParams) Deeplink() (deeplinks.Deeplink, error) {
+	q, err := p.Query()
+	if err != nil {
+		return deeplinks.Deeplink{}, err
+	}
+	return deeplinks.New("passport", q), nil
+}
+
+// URI generates request URI.
+func (p *RequestParams) URI() (string, error) {
+	dl, err := p.Deeplink()
+	if err != nil {
+		return "", err
+	}
+	return dl.TG(true), nil
+}
