5388218c11626db7cc17ddc6515aa5f46d7cc8f8
karalef <mgsiask@gmail.com>
2025-07-28 12:51:02 +0300

    add updates.GenerateSecret

--- updates/webhook.go
+++ updates/webhook.go
@@ -1,12 +1,39 @@
 package updates
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"io"
 	"net/http"
 
 	"github.com/karalef/tgot/api"
 	"github.com/karalef/tgot/api/tg"
 )
+
+// Encoding is an interface for encoding.
+type Encoding interface {
+	EncodeToString([]byte) string
+	DecodedLen(int) int
+}
+
+// GenerateSecret generates random secret token of specified length.
+// It uses the provided encoding (or base64 without padding by default) to get
+// the string representation of the secret.
+// If length is 0 or bigger than 128, it will be set to 64.
+func GenerateSecret(enc Encoding, length uint8) string {
+	if enc == nil {
+		enc = base64.RawStdEncoding
+	}
+	if length == 0 || length > 128 {
+		length = 64
+	}
+	buf := make([]byte, enc.DecodedLen(int(length)))
+	_, err := rand.Read(buf)
+	if err != nil {
+		panic("error while generating secret: " + err.Error())
+	}
+	return enc.EncodeToString(buf)
+}
 
 // WebhookHandler is a handler for telegram webhook requests.
 type WebhookHandler struct {
